f75f71b8c6362aba3f902a61ede4dc6ad239dbaf
husneara <husnearaa99@gmail.com>
2023-12-03 20:03:51 +0600

    has done get product

--- index.js
+++ index.js
@@ -30,12 +30,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const productCollection = client.db("tecthubDB").collection("products");
+
+
+    app.get('/products', async(req, res) =>{
+        const result = await productCollection.find().sort({ timestamp: -1 }).toArray();
+        res.send(result);
+    })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
