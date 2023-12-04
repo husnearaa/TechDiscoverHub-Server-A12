cb0ebcaf00aefa8a1204c18b23bde58c1e3648aa
husneara <husnearaa99@gmail.com>
2023-12-04 20:24:21 +0600

    had dome details page method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -36,19 +36,19 @@ async function run() {
 
     app.get('/products', async (req, res) => {
       const result = await productCollection.find().toArray();
-      console.log(result);
+      // console.log(result);
       res.send(result);
     });
 
 
 
-    // app.get("/products/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   console.log(id);
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await productCollection.findOne(query);
-    //   res.send(result);
-    // });
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
 
 
